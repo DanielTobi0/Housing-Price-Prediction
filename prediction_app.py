4e0ae596d86d5b9d546896b481a46009e539ce4d
Daniel Tobi <94564307+DanielTobi0@users.noreply.github.com>
2022-09-16 02:13:32 +0100

    Update prediction_app.py

--- prediction_app.py
+++ prediction_app.py
@@ -13,7 +13,7 @@ import math
 
 
 # loading the saved model
-loaded_model = pickle.load(open('/prediction_model.sav','rb'))
+loaded_model = pickle.load(open('prediction_model.sav','rb'))
 
 st.info(
         f""" 
